917f16be7d8fe23247416ca92864a4e060a4875a
Francis Tan <kian080787@gmail.com>
2022-12-16 18:05:57 +0800

    delete files

--- practice/join.py
+++
@@ -1,9 +0,0 @@
-# using on string:
-s = "我叫譚庚豪，我愛寫程式。"
-new_s = " ".join(s)
-# print(new_s)
-
-# using on string list:
-lst = ['我', '是', '譚', '庚', '豪', '。']
-res = "".join(lst)
-# print(res)
--- practice/sort_str.py
+++
@@ -1,7 +0,0 @@
-def sort_str(str_lst):
-    print(min(str_lst))
-    print(max(str_lst))
-
-
-lst = ["be", "d"]
-sort_str(lst)
